f0cf162d299c2078a07d816c84b83fd0081575f6
Beixi Lu <beixi_lu@yahoo.com>
2020-03-19 20:48:49 +0100

    m

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning user database'
+User.destroy_all
+
+puts 'creating Users'
+#file = URI.open('https://res.cloudinary.com/dvpcxhofq/image/upload/v1583243051/Philipp_Boston_kdjdty.jpg')
+user = User.create!(
+  first_name: "amy",
+  last_name: "lu",
+  email: "amy.lu@gmail.com",
+  username: "Amy",
+  password: "000000",
+  # birthday: Date.new(1984,11,29),
+  # bio: "Everything under control",
+  # interest: "Music"
+  )
+#user.photo.attach(io: file, filename: 'photo')
+
+
+puts 'Cleaning dishes database'
+Dish.destroy_all
+
+puts 'creating Dishes'
+dish = Dish.create!(
+  name: "麻婆豆腐/Ma Po Tofu",
+  description: 'This mapo tofu recipe is the true blue, authentic real deal—the spicy, tongue-numbing, rice-is-absolutely-not-optional mapo tofu that you get in the restaurants!',
+  user_id: User.first.id
+  )
